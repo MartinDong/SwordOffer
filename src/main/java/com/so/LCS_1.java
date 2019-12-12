71f52fe0bc459cfd24e79d1d61a50c5d9ab669de
董宏宇 <martindhy@gmail.com>
2019-12-12 16:32:12 +0800

    增加了：求两个字符串的最长公共子串
    https://blog.csdn.net/yuming226/article/details/94360852

---
+++ src/main/java/com/so/LCS_1.java
@@ -0,0 +1,35 @@
+package com.so;
+
+public class LCS_1 {
+    public static void main(String[] args) {
+        LCS_1 lcs = new LCS_1();
+        String str = lcs.getLSC("acbcbcef", "abscbced");
+        System.out.println(str);
+    }
+
+    private String getLSC(String A, String B) {
+        int[][] dp = new int[B.length()][A.length()];
+        int end_index = 0;
+        int maxLength = 0;
+        for (int i = 0; i < A.length(); i++) {
+            for (int j = 0; j < B.length(); j++) {
+                if (B.charAt(j) == A.charAt(i)) {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = dp[i - 1][j - 1] + 1;
+                    }
+
+                    if (dp[i][j] > maxLength) {
+                        maxLength = dp[i][j];
+                        end_index = j;
+                    }
+                }
+            }
+        }
+        System.out.println("maxLength = " + maxLength);
+        System.out.println("end_index = " + end_index);
+        end_index += 1;
+        return B.substring(end_index - maxLength, end_index);
+    }
+}
